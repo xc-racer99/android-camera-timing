21db53fdcf850bb32f61036f34038807fbe066bb
xc-racer99 <xc-racer2@live.ca>
2017-12-31 18:30:23 -0800

    Cleanup
    Make a variable local, remove an unused method and an unused variable, keep trying on the orientation woes...

--- Application/src/main/java/ca/skilarchhills/android/cameratiming/CameraActivity.java
+++ Application/src/main/java/ca/skilarchhills/android/cameratiming/CameraActivity.java
@@ -102,7 +102,7 @@ public class CameraActivity extends Activity implements View.OnClickListener, Ad
                 SensorManager.SENSOR_DELAY_NORMAL) {
             public void onOrientationChanged (int orientation) {
                 if(orientation > 315 && orientation < 45)
-                    mOrientation = ExifInterface.ORIENTATION_NORMAL;
+                    mOrientation = ExifInterface.ORIENTATION_UNDEFINED;
                 else if(orientation >= 45 && orientation <= 135)
                     mOrientation = ExifInterface.ORIENTATION_ROTATE_90;
                 else if(orientation > 135 && orientation <= 225)
@@ -314,11 +314,6 @@ public class CameraActivity extends Activity implements View.OnClickListener, Ad
     private ImageReader mImageReader;
 
     /**
-     * This is the output file for our picture.
-     */
-    private File mFile;
-
-    /**
      * {@link CaptureRequest.Builder} for the camera preview
      */
     private CaptureRequest.Builder mPreviewRequestBuilder;
@@ -375,55 +370,6 @@ public class CameraActivity extends Activity implements View.OnClickListener, Ad
         }
 
     };
-
-    /**
-     * Given {@code choices} of {@code Size}s supported by a camera, choose the smallest one that
-     * is at least as large as the respective texture view size, and that is at most as large as the
-     * respective max size, and whose aspect ratio matches with the specified value. If such size
-     * doesn't exist, choose the largest one that is at most as large as the respective max size,
-     * and whose aspect ratio matches with the specified value.
-     *
-     * @param choices           The list of sizes that the camera supports for the intended output
-     *                          class
-     * @param textureViewWidth  The width of the texture view relative to sensor coordinate
-     * @param textureViewHeight The height of the texture view relative to sensor coordinate
-     * @param maxWidth          The maximum width that can be chosen
-     * @param maxHeight         The maximum height that can be chosen
-     * @param aspectRatio       The aspect ratio
-     * @return The optimal {@code Size}, or an arbitrary one if none were big enough
-     */
-    private static Size chooseOptimalSize(Size[] choices, int textureViewWidth,
-                                          int textureViewHeight, int maxWidth, int maxHeight, Size aspectRatio) {
-
-        // Collect the supported resolutions that are at least as big as the preview Surface
-        List<Size> bigEnough = new ArrayList<>();
-        // Collect the supported resolutions that are smaller than the preview Surface
-        List<Size> notBigEnough = new ArrayList<>();
-        int w = aspectRatio.getWidth();
-        int h = aspectRatio.getHeight();
-        for (Size option : choices) {
-            if (option.getWidth() <= maxWidth && option.getHeight() <= maxHeight &&
-                    option.getHeight() == option.getWidth() * h / w) {
-                if (option.getWidth() >= textureViewWidth &&
-                        option.getHeight() >= textureViewHeight) {
-                    bigEnough.add(option);
-                } else {
-                    notBigEnough.add(option);
-                }
-            }
-        }
-
-        // Pick the smallest of those big enough. If there is no one big enough, pick the
-        // largest of those not big enough.
-        if (bigEnough.size() > 0) {
-            return Collections.min(bigEnough, new CompareSizesByArea());
-        } else if (notBigEnough.size() > 0) {
-            return Collections.max(notBigEnough, new CompareSizesByArea());
-        } else {
-            Log.e(TAG, "Couldn't find any suitable preview size");
-            return choices[0];
-        }
-    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
@@ -763,7 +709,7 @@ public class CameraActivity extends Activity implements View.OnClickListener, Ad
 
         Bitmap pic = mTextureView.getBitmap();
 
-        mFile = new File(getExternalFilesDir(null), time + ".jpg");
+        File mFile = new File(getExternalFilesDir(null), time + ".jpg");
 
         FileOutputStream output = null;
         try {
@@ -780,8 +726,6 @@ public class CameraActivity extends Activity implements View.OnClickListener, Ad
             if (null != output) {
                 try {
                     output.close();
-                    long sizeBytes = mFile.length();
-
                     // Now send the actual file
                     if(networkTask != null)
                         networkTask.queue.add(mFile);
