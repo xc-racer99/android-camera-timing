74696a3ca07715db09c71caa5d0deeb1baf00e25
xc-racer99 <xc-racer2@live.ca>
2018-01-03 17:53:15 -0800

    WIP Use new network protocol
    Not working on client side yet...

--- Application/src/main/java/ca/skilarchhills/android/cameratiming/SocketService.java
+++ Application/src/main/java/ca/skilarchhills/android/cameratiming/SocketService.java
@@ -1,37 +1,69 @@
 package ca.skilarchhills.android.cameratiming;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Binder;
 import android.os.IBinder;
 import android.os.SystemClock;
 import android.util.Log;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Created by jon on 03/01/18.
+ * Service class for all TCP communications with the PC
  */
 
 public class SocketService extends Service {
     private static final String TAG = "CameraTimingSocket";
 
+    // Error messages
+    private static final int NO_DATA = 1;
+    private static final int NOT_IMPLEMENTED = 2;
+
+    // Commands that the phone can send
+    private static final int PHONE_IMAGE = 1001;
+    private static final int PHONE_WITH_SECOND_IMAGE = 1002;
+
+    // Commands that the PC can send
+    private static final int PC_ACK = 2001;
+    private static final int PC_REQUEST_NEXT = 2002;
+    private static final int PC_REQUEST_SPECIFIC = 2003;
+    private static final int PC_REQUEST_ALL = 2004;
+
+    // For setup of service
     private final IBinder mBinder = new MyBinder();
     public static final String initialPicsTag = "ca.skilarchhills.android.cameratiming.InitialPics";
     public static final String picsTag = "ca.skilarchhills.android.cameratiming.NewPics";
 
+    // For the network server itself
     private boolean serviceClosing = false;
+    final int portNum = 54321;
+    ServerSocket listener;
     private Socket socket;
+    BufferedInputStream nis;
+    BufferedOutputStream nos;
 
-    private LinkedList<String> queue = new LinkedList<>();
+    // To determine if wifi is enabled or not
+    private WifiManager wifiMan;
+
+    // Contains all of the images we know of
+    private CopyOnWriteArrayList<String> queue = new CopyOnWriteArrayList<>();
+    private int nextIndex = -1;
 
     @Override
     public void onCreate() {
@@ -43,6 +75,8 @@ public class SocketService extends Service {
         });
         thread.start();
         Log.d(TAG, "Thread started");
+
+        wifiMan = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
     }
 
     @Override
@@ -52,24 +86,27 @@ public class SocketService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        String[] initialPics = intent.getStringArrayExtra(initialPicsTag);
-
-        // Add initial pictures
-        if(initialPics != null && initialPics.length > 0)
-            Collections.addAll(queue, initialPics);
-
         // Used to add an additional picture
         String nextPic = intent.getStringExtra(picsTag);
         if(nextPic != null)
             queue.add(nextPic);
+
+        Log.d(TAG, "onStartCommand run");
 
         return Service.START_NOT_STICKY;
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // Used to add additional pictures
-        queue.add(intent.getStringExtra(picsTag));
+        String[] initialPics = intent.getStringArrayExtra(initialPicsTag);
+
+        // Add initial pictures
+        if(initialPics != null && initialPics.length > 0)
+            Collections.addAll(queue, initialPics);
+
+        nextIndex = queue.size() - 1;
+
+        Log.d(TAG, "onBind run");
 
         return mBinder;
     }
@@ -80,6 +117,9 @@ public class SocketService extends Service {
         }
     }
 
+    /**
+     * Converts a long to a byte array for sending over the network, endian independent
+     */
     private static byte[] longToBytes(long l) {
         byte[] result = new byte[8];
         for (int i = 7; i >= 0; i--) {
@@ -89,69 +129,214 @@ public class SocketService extends Service {
         return result;
     }
 
+    /**
+     * Converts a byte array to a long, endian independent
+     */
+    public long bytesToLong(byte[] b) {
+        long result = 0;
+        for (int i = 0; i < 8; i++) {
+            result <<= 8;
+            result |= (b[i] & 0xFF);
+        }
+        return result;
+    }
+
+    /**
+     * Convenience method to make sure wifi is enabled and connected
+     */
+    private boolean isWifiConnected() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork;
+        if(connectivityManager != null) {
+            activeNetwork = connectivityManager.getActiveNetworkInfo();
+            if(activeNetwork != null) {
+                if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI)
+                    return true;
+            }
+        }
+        return false;
+
+        /*
+
+        Log.e(TAG, "Check if wifi is connected");
+
+        if(wifiMan != null && wifiMan.isWifiEnabled()) {
+            Log.e(TAG, "got here");
+            WifiInfo wifiInfo = wifiMan.getConnectionInfo();
+            if( wifiInfo.getNetworkId() == -1 ) {
+                return true;
+            }
+        }
+        return false;
+
+        */
+    }
+
+    /**
+     * Reads a long from socket
+     * @return next 8 bytes as long from socket, or -1 on error
+     */
+    private long readLong() {
+        int bytesRead = 0;
+        byte[] buffer = new byte[8];
+
+        try {
+            while (nis.available() < 8) {
+                SystemClock.sleep(50);
+                if (!socket.getInetAddress().isReachable(2000))
+                    return -1;
+            }
+
+            // We now have at least 8 bytes, hopefully
+            bytesRead = nis.read(buffer);
+            if(bytesRead != 8) {
+                // Shouldn't happen...
+                return -1;
+            }
+            return bytesToLong(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    /**
+     * Waits for the PC to acknowledge receipt of the packet
+     * Returns true if ACK received, false if times out
+     */
+    private boolean waitForAck() {
+        int ret = (int)readLong();
+        return ret == PC_ACK;
+    }
+
+    /**
+     * Sends an error over the network stream
+     * Returns true if successfully sent data, false if IO Error
+     */
+    private boolean sendError(int error) {
+        // FIXME - Implement
+        return true;
+    }
+
+    /**
+     * Sends a file across the socket
+     * @param file File to send
+     * @param fileIndex File number
+     * @return Success
+     */
+    private boolean sendFile(File file, int fileIndex) {
+        try {
+            Log.v(TAG, "Sending file");
+
+            long numBytes = file.length();
+            long timestamp = 0;
+            String[] parts = file.getName().split("\\.(?=[^\\.]+$)");
+            if (parts.length > 0) {
+                timestamp = Long.parseLong(parts[0]);
+            }
+
+            nos.write(longToBytes(PHONE_IMAGE));
+            nos.write(longToBytes(fileIndex));
+            nos.write(longToBytes(timestamp));
+            nos.write(longToBytes(numBytes));
+
+            // Copy the file to a buffer that is half the size of file (because long.size/2=int.size)
+            FileInputStream fis = new FileInputStream(file);
+            int bufSize = (int) (numBytes / 2);
+            byte[] buf = new byte[bufSize];
+            for (int i = 0; i < 2; i++) {
+                if (!socket.getInetAddress().isReachable(2000)) {
+                    return false;
+                }
+
+                if (fis.read(buf, 0, bufSize) != -1)
+                    nos.write(buf);
+            }
+            // If we had an odd number, then there's still one byte to go
+            if (numBytes % 2 != 0) {
+                if (!socket.getInetAddress().isReachable(2000)) {
+                    return false;
+                }
+
+                nos.write(fis.read());
+            }
+            nos.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    /**
+     * Starts a network server if wifi is enabled that the PC can connect to
+     */
     private void startNetwork() {
-        final int portNum = 54321;
-        ServerSocket listener;
-        InputStream nis;
-        BufferedOutputStream nos;
+        // Wait until wifi is connected
+        while(!isWifiConnected())
+            SystemClock.sleep(5000);
+
         try {
             listener = new ServerSocket(portNum);
             Log.d(TAG, String.format("Listening on port %d", portNum));
             Log.d(TAG, "Waiting for client to connect");
             socket = listener.accept();
             if (socket.isConnected()) {
-                nis = socket.getInputStream();
+                nis = new BufferedInputStream(socket.getInputStream());
                 nos = new BufferedOutputStream(socket.getOutputStream());
                 Log.i(TAG, "doInBackground: Socket created, streams assigned");
                 Log.i(TAG, "doInBackground: Waiting for initial data");
 
-                File file;
+                int fileIndex = -1;
+
                 while (socket.isConnected() && !socket.isClosed() && !serviceClosing) {
                     // Make sure we can contact our client, if not, then break
                     if(!socket.getInetAddress().isReachable(2000))
                         break;
 
-                    String temp = queue.poll();
-                    if (temp == null) {
-                        SystemClock.sleep(2000);
-                        continue;
+                    // Read 8 command from PC
+                    long cmd = readLong();
+                    if(cmd == -1) {
+                        // Error reading data
+                        break;
                     }
 
-                    file = new File(temp);
-
-                    Log.v(TAG, "doInBackground: Sending file");
-
-                    long numBytes = file.length();
-                    long timestamp = 0;
-                    String[] parts = file.getName().split("\\.(?=[^\\.]+$)");
-                    if (parts.length > 0) {
-                        timestamp = Long.parseLong(parts[0]);
-                    }
-
-                    nos.write(longToBytes(timestamp));
-                    nos.write(longToBytes(numBytes));
-
-                    // Copy the file to a buffer that is half the size of file (because long.size/2=int.size)
-                    FileInputStream fis = new FileInputStream(file);
-                    int bufSize = (int) (numBytes / 2);
-                    byte[] buf = new byte[bufSize];
-                    for (int i = 0; i < 2; i++) {
-                        if(!socket.getInetAddress().isReachable(2000)) {
+                    File file;
+                    switch((int)cmd) {
+                        case PC_REQUEST_NEXT:
+                            while(queue.isEmpty()) {
+                                SystemClock.sleep(2000);
+                                if(!socket.getInetAddress().isReachable(2000))
+                                    break;
+                            }
+                            fileIndex = nextIndex;
+                            file = new File(queue.get(nextIndex));
                             break;
-                        }
-
-                        if(fis.read(buf, 0, bufSize) != -1)
-                            nos.write(buf);
-                    }
-                    // If we had an odd number, then there's still one byte to go
-                    if (numBytes % 2 != 0) {
-                        if(!socket.getInetAddress().isReachable(2000)) {
+                        case PC_REQUEST_SPECIFIC:
+                            int index = (int)readLong();
+                            if(queue.size() - 1 > index) {
+                                sendError(NO_DATA);
+                                waitForAck();
+                                continue;
+                            }
+                            fileIndex = index;
+                            file = new File(queue.get(index));
                             break;
-                        }
-
-                        nos.write(fis.read());
+                        case PC_REQUEST_ALL:
+                        default:
+                            // Not implemented
+                            sendError(NOT_IMPLEMENTED);
+                            waitForAck();
+                            continue;
                     }
-                    nos.flush();
+
+                    if(!socket.getInetAddress().isReachable(2000))
+                        break;
+
+                    if(!sendFile(file, fileIndex))
+                        break;
+
+                    if(!waitForAck())
+                        break;
                 }
 
                 // Close socket
@@ -181,6 +366,6 @@ public class SocketService extends Service {
     }
 
     public boolean isConnected() {
-        return socket.isConnected();
+        return socket != null && socket.isConnected();
     }
 }
